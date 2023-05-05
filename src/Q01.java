0fd58f2576d2abf766b485a0afef817e23bbefea
Banuka Dananjaya <61711700+bannukadananjaya@users.noreply.github.com>
2023-05-05 12:21:25 +0530

    Lab2question 01 start

---
+++ src/Q01.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q01 {
+
+    public void find(int n,int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (n == arr[i]) {
+                System.out.println("Value find");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int array[] = {2,5,6,10,11,13,15,18,20,22,23,25};
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        Q01 one = new Q01();
+        one.find(num,array);
+    }
+}
